53412d4b09cf7d4b507ce1c83ec6ccf7b6fa6653
Wahyudi Prasidhatama <wahyudiprasidhatama@gmail.com>
2022-09-20 10:06:14 +0700

    Update practice_case1_using_python.py

--- practice_case1_using_python.py
+++ practice_case1_using_python.py
@@ -1,3 +1,4 @@
+# import package safely
 try :
     from google.cloud import storage
 except Exception as e:
@@ -6,10 +7,13 @@ except Exception as e:
 # Create a client object by reading the key, key should be in the same folder as this file
 storage_client = storage.Client.from_service_account_json('feisty-outlet-362916-29300cba76c1.json')
 
-# Define the bucket and file ino
+# Define the bucket and filename info
 BUCKET_NAME = 'fellowship7'
 FOLDERNAME = 'practice-case-1'
 FILENAME = 'file-in-GCP.txt'
+
+# Define the file that'll be uploaded
+UPLOADED_FILENAME = 'uploaded-file.txt'
 
 # Create a bucket object
 bucket = storage_client.get_bucket(BUCKET_NAME)
@@ -22,7 +26,7 @@ bucketpath = '{}/{}'.format(FOLDERNAME,FILENAME)
 blob = bucket.blob(bucketpath)
 
 # Using with, to open and close the file safely within the function only
-with open('uploaded-file.txt', mode= 'r') as f :
+with open(UPLOADED_FILENAME, mode= 'r') as f :
     blob.upload_from_file(f)
     
 print('Upload Completed')
